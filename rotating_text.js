81765d9d3ed3dc239d6225ebede22104bfcb1e41
Sandor Tudja <47197537+GEAR-2012@users.noreply.github.com>
2020-04-10 02:16:19 +0100

    adding text orientation possibility

--- rotating_text.js
+++ rotating_text.js
@@ -1,5 +1,5 @@
 // 9 April 2020, Kemnay, UK
-// Author: Sandor Tudja, (GEAR web development (c)2012)
+// (c)2020, GEAR web development. All rights reserved.
 
 // *************
 
@@ -24,6 +24,14 @@ function containerDeploying(x) {
   let rotInc = 300 / len; // 360 - 60 so there is 60deg gap
   let content = "";
 
+  // check for the letter orientation
+  let rotLetters = 0;
+  let isInside = mainContainers[x].classList.contains("inside");
+  if (isInside === true) {
+    textArray.reverse();
+    rotLetters = 180;
+  }
+
   // making all letters in a letter_holder
   for (let i = 0; i < len; i++) {
     let letter = textArray[i];
@@ -33,10 +41,11 @@ function containerDeploying(x) {
     content += "display: inline-block;";
     content += "position: absolute;";
     content += "transform-origin: 50% 100%;";
-    content += "bottom: 50%; left: 48%;";
+    content += "bottom: 50%; left: 47%;";
     content += "transform: rotate(" + Math.round(rot) + "deg);";
     content += "'>";
     content += "<p class='letter' style='";
+    content += "transform: rotate(" + rotLetters + "deg);";
     content += "font-family: monospace;"; // font-family
     content += "'>" + letter + "</p>";
     content += "</div>";
@@ -56,11 +65,12 @@ setInterval(function () {
   let rotateInc = 0.5; // step incremet of rotation (positive number)
 
   for (let i = 0; i < len; i++) {
-    let isCw = allLetterHolders[i].parentElement.classList.contains("cw");
     let rotBefore = parseFloat(
       allLetterHolders[i].style.transform.slice(7, -4)
     );
 
+    // check for the direction of rotation
+    let isCw = allLetterHolders[i].parentElement.classList.contains("cw");
     if (isCw === true) {
       rotate = rotBefore += rotateInc;
     } else if (isCw === false) {
